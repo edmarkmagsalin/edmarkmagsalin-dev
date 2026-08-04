cc40885aa225e0cfeaefa2f42b37e95a871f9a0a
Edmark Magsalin <emagsalin@cainc.com>
2026-08-05 02:44:19 +0800

    feat: about updates

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,7 +13,7 @@ const About = () => {
                                 About
                             </h2>
                             <p>
-                                My experience on web developement is over 5 years now, I started learning web development back in 2017. I started with HTML, CSS and PHP. Then I learned JavaScript and Node.js. I also learned React and TypeScript.
+                                My experience on web developement is over 9 years now, I started learning web development back in 2017. I started with HTML, CSS and PHP. Then I learned JavaScript and Node.js. I also learned React and TypeScript.
                             </p>
                             <p>
                                 I'm a graduate of Bachelor of Science in Information Technology and I build things that can make people lives easier and better. I'm a problem solver, listens carefully to the requirements needed in a project.
